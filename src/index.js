d0014f44f932663f8a294729bbf9627ea8277ed8
FranGutierrez <55754815+FranGutierrez@users.noreply.github.com>
2022-05-06 19:00:59 -0300

    TG: Tasks - Get all & get by id

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ app.get('/admins', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// console.log(`Example app listening on port ${port}`);
 });
--- src/resources/tasks.js
+++ src/resources/tasks.js
@@ -1,20 +1,21 @@
 const express = require('express');
 
 const tasks = require('../data/tasks.json');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(tasks);
+  res.send(tasks);
 });
 
 router.get('/:id', (req, res) => {
-    const taskID = req.params.id;
-    const task = tasks.find((task) => task.id == taskID);
-    if (task) {
-        res.send(task);
-    } else {
-        res.send('Task not found');
-    }
+  const taskID = req.params.id;
+  const task = tasks.find((t) => t.id === taskID);
+  if (task) {
+    res.send(task);
+  } else {
+    res.send('Task not found');
+  }
 });
 
 module.exports = router;
