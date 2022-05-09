18e2ee65b1d63d16952e24fdea61fd14193bded6
AnaMurinigo <anamurinigo@gmail.com>
2022-05-09 19:26:43 -0300

    TG: Employee GET corrections and new filters

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -10,8 +10,26 @@ router.get('/:id', (req, res) => {
   const idEmployee = Number(req.params.id);
   res.send(employees.find((employee) => employee.id === idEmployee) || 'Void');
 });
-// router.delete('/delete/:id', (req,res) => {
+// GET BY Name
+router.get('/firstName/:name', (req, res) => {
+  const nameEmployee = String(req.params.name);
+  res.send(employees.filter((employee) => employee.firstName === nameEmployee));
+});
+// GET BY LastName
+router.get('/lastName/:lastName', (req, res) => {
+  const lastNameEmployee = String(req.params.lastName);
+  res.send(employees.filter((employee) => employee.lastName === lastNameEmployee));
+});
+// GET BY Email
+router.get('/email/:email', (req, res) => {
+  const emailEmployee = String(req.params.email);
+  res.send(employees.filter((employee) => employee.email === emailEmployee));
+});
+
+// router.delete('/delete/:id', (req, res) => {
 //   const idEmployee = Number(req.params.id);
 //   const selectedEmployee = employees.find((employee) => employee.id === idEmployee);
 // });
 module.exports = router;
+// shift alt flechita
+// sobre la seleccion ctrl d
