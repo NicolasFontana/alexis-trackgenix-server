95ac394bd61e3ed7d7851b0d82bafa46a5b4b2ad
Javier Biselli <javierbiselli@gmail.com>
2022-05-09 10:12:24 -0300

    TG: Added body parser and routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // use "import" to import libraries
-import express from 'express';
+const express = require('express');
 
 // use "require" to import JSON files
 const admins = require('./data/admins.json');
+
+// routes
+const routes = require('./resources/super-admins');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,6 +19,10 @@ app.get('/admins', (req, res) => {
     data: admins,
   });
 });
+
+app.use(express.json());
+app.use('/super-admins', routes);
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
