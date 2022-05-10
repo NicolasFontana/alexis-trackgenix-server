b85d561d8f24ef0c26f5dc640a02db9ecc49b2e6
Fernando Carbone <fernando.carbone707@gmail.com>
2022-05-09 22:16:00 -0300

    TG: PUT method assigning employee with a role from Project

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -71,4 +71,87 @@ router.put('/id/:id', (req, res) => {
     res.status(400).json({ msg: `No project with the id of ${req.params.id}` });
   }
 });
+
+// Assigning a new employee with Dev role
+router.put('/assign/dev/:id', (req, res) => {
+  const projectRole = projects.find((project) => project.id === Number(req.params.id));
+  if (projectRole !== undefined) {
+    const devs = projects.filter((project) => project.devs === Number(req.params.devs));
+    const newEmployee = {
+      devs: (devs || projectRole),
+    };
+    projectRole.devs.push(req.body.devs);
+    devs.push(req.body);
+    res.send(newEmployee);
+  } else {
+    res.status(400).json({ msg: `No valid project with the id of ${req.params.id}` });
+  }
+});
+
+// Assigning a new employee with Qas role
+router.put('/assign/qas/:id', (req, res) => {
+  const projectRole = projects.find((project) => project.id === Number(req.params.id));
+  if (projectRole !== undefined) {
+    const qas = projects.filter((project) => project.qas === Number(req.params.qas));
+    const newEmployee = {
+      qas: (qas || projectRole),
+    };
+    projectRole.qas.push(req.body.qas);
+    qas.push(req.body);
+    res.send(newEmployee);
+  } else {
+    res.status(400).json({ msg: `No valid project with the id of ${req.params.id}` });
+  }
+});
+
+// Assigning a new employee with PM role
+router.put('/assign/pm/:id', (req, res) => {
+  const projectRole = projects.find((project) => project.id === Number(req.params.id));
+  if (projectRole !== undefined) {
+    const projectManager = projects.filter((project) => project.projectManager
+    === Number(req.params.projectManager));
+    const newEmployee = {
+      projectManager: (projectManager || projectRole),
+    };
+    projectRole.projectManager.push(req.body.projectManager);
+    projectManager.push(req.body);
+    res.send(newEmployee);
+  } else {
+    res.status(400).json({ msg: `No valid project with the id of ${req.params.id}` });
+  }
+});
+
+// Assigning a new employee with tl role
+router.put('/assign/tl/:id', (req, res) => {
+  const projectRole = projects.find((project) => project.id === Number(req.params.id));
+  if (projectRole !== undefined) {
+    const techLeader = projects.filter((project) => project.techLeader
+    === Number(req.params.techLeader));
+    const newEmployee = {
+      techLeader: (techLeader || projectRole),
+    };
+    projectRole.techLeader.push(req.body.techLeader);
+    techLeader.push(req.body);
+    res.send(newEmployee);
+  } else {
+    res.status(400).json({ msg: `No valid project with the id of ${req.params.id}` });
+  }
+});
+
+// Assigning a new employee with admin role
+router.put('/assign/admin/:id', (req, res) => {
+  const projectRole = projects.find((project) => project.id === Number(req.params.id));
+  if (projectRole !== undefined) {
+    const admin = projects.filter((project) => project.admin
+    === Number(req.params.admin));
+    const newEmployee = {
+      admin: (admin || projectRole),
+    };
+    projectRole.admin.push(req.body.admin);
+    admin.push(req.body);
+    res.send(newEmployee);
+  } else {
+    res.status(400).json({ msg: `No valid project with the id of ${req.params.id}` });
+  }
+});
 module.exports = router;
