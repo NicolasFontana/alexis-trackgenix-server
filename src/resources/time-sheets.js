956e452efe51bb3fc2e1f3cec8f1c4248dab4612
Karen Agustina Soto <karenagustinams@gmail.com>
2022-05-10 00:49:01 -0300

    TG: Modified CREATE so it generates a new ID automatically

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,39 +1,70 @@
+// IMPORT & REQUIRES
 const express = require('express');
 const fs = require('fs');
-const timesheet = require('../data/time-sheets.json');
 
 const router = express.Router();
+
+// DATA IN JSON
+const timesheet = require('../data/time-sheets.json');
 
 router.get('/', (req, res) => {
   res.send(timesheet);
 });
 
+// TO SEE A TIMESHEET BY ID
 router.get('/id/:id', async (req, res) => {
   const timesheetID = req.params.id.toString();
   const response = timesheet.find((elem) => elem.id.toString() === timesheetID);
   if (response) {
     res.send(response);
   } else {
-    res.send('Timesheet not found');
+    res.send(`Timesheet ${timesheetID} not found`);
   }
 });
 
+// TO SEE A TIMESHEET BY ROLE
 router.get('/role/:role', async (req, res) => {
   const timesheetRole = req.body.role.toString();
-  const response = timesheet.filter((elem) => elem.id.toString() === timesheetRole);
+  const response = timesheet.filter((elem) => elem.role.toString() === timesheetRole);
   if (response) {
     res.send(response);
   } else {
-    res.send('Role not found');
+    res.send(`Role ${timesheetRole} not found`);
   }
 });
 
+// TO SEE A TIMESHEET BY TASK
+router.get('/task/:task', async (req, res) => {
+  const timesheetTask = req.params.task.toString();
+  const response = timesheet.find((elem) => elem.task.toString() === timesheetTask);
+  if (response) {
+    res.send(response);
+  } else {
+    res.send(`Task ${timesheetTask} not found`);
+  }
+});
+
+// TO CREATE A NEW TIMESHEET
 router.post('/create', (req, res) => {
-  const timesheetData = req.body;
+  const {
+    description, date, task, validated, employee, projectId, projectManager, role,
+  } = req.body;
+  const id = timesheet.reduce((prev, curr) => (prev <= curr.id ? curr.id + 1 : prev), 0);
+  const timesheetData = {
+    id,
+    description,
+    date,
+    task,
+    validated,
+    employee,
+    projectId,
+    projectManager,
+    role,
+  };
   timesheet.push(timesheetData);
-  fs.writeFile('src/data/time-sheets.js', JSON.stringify(timesheet), (err) => {
+  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timesheet), (err) => {
     if (err) {
-      res.send(err);
+      res.send(400);
     } else {
       res.send('Timesheet created.');
     }
@@ -47,7 +78,7 @@ router.delete('/delete/:id', (req, res) => {
   if (timesheet.length === filteredTimesheet.length) {
     res.send('Could NOT delete timesheet because it was not found');
   } else {
-    fs.writeFile('src/data/time-sheets.js', JSON.stringify(filteredTimesheet), (err) => {
+    fs.writeFile('src/data/time-sheets.json', JSON.stringify(filteredTimesheet), (err) => {
       if (err) {
         res.send(err);
       } else {
