7457fe2fc24e9714d2bf31619eeb22b3e6544fd8
AnaMurinigo <anamurinigo@gmail.com>
2022-05-10 19:15:44 -0300

    TG: Corrections 2.0. Path equals to the rest of the team

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -6,35 +6,37 @@ const router = express.Router();
 // GET ALL
 router.get('/', (req, res) => res.send(employees));
 // GET BY ID
-router.get('/:id', (req, res) => {
+router.get('/id=:id', (req, res) => {
   const idEmployee = Number(req.params.id);
   res.send(employees.find((employee) => employee.id === idEmployee) || 'Null');
 });
 // GET BY Name
-router.get('/firstName/:name', (req, res) => {
+router.get('/firstName=:name', (req, res) => {
   const nameEmployee = String(req.params.name);
-  res.send(employees.filter((employee) => employee.firstName === nameEmployee));
+  res.send(employees.filter((employee) => employee.firstName.toLowerCase()
+  === nameEmployee.toLowerCase()));
 });
 // GET BY LastName
-router.get('/lastName/:lastName', (req, res) => {
+router.get('/lastName=:lastName', (req, res) => {
   const lastNameEmployee = String(req.params.lastName);
-  res.send(employees.filter((employee) => employee.lastName === lastNameEmployee));
+  res.send(employees.filter((employee) => employee.lastName.toLowerCase()
+  === lastNameEmployee.toLowerCase()));
 });
 // GET BY Email
-router.get('/email/:email', (req, res) => {
+router.get('/email=:email', (req, res) => {
   const emailEmployee = String(req.params.email);
   res.send(employees.filter((employee) => employee.email === emailEmployee));
 });
 // GET BY Active
-router.get('/active/:active', (req, res) => {
+router.get('/active=:active', (req, res) => {
   const activeEmployee = Boolean(req.params.active);
   res.send(employees.filter((employee) => employee.active === activeEmployee));
 });
 // DELETE Employee
-router.delete('/delete/:id', (req, res) => {
+router.delete('/delete=:id', (req, res) => {
   const idEmployee = Number(req.params.id);
   const withoutEmployee = employees.filter((employee) => employee.id !== idEmployee);
-  if (withoutEmployee === employees) {
+  if (withoutEmployee.length === employees.length) {
     res.status(400).json({ msg: 'Please fill in a valid id' });
   } else {
     fs.writeFile('src/data/employees.json', JSON.stringify(withoutEmployee), (err) => {
@@ -77,17 +79,17 @@ router.post('/', (req, res) => {
   }
 });
 // UPDATE Employee
-router.put('/update/:id', (req, res) => {
+router.put('/update=:id', (req, res) => {
   const idEmployee = Number(req.params.id);
-  const employeeToUpdate = employees.some((employee) => employee.id === idEmployee);
+  const employeeToUpdate = employees.find((employee) => employee.id === idEmployee);
   const withoutOldEmployee = employees.filter((employee) => employee.id !== idEmployee);
   const newEmployee = {
     id: idEmployee,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    phone: req.body.phone,
-    email: req.body.email,
-    active: req.body.active,
+    firstName: (req.body.firstName || employeeToUpdate.firstName),
+    lastName: (req.body.lastName || employeeToUpdate.lastName),
+    phone: (req.body.phone || employeeToUpdate.phone),
+    email: (req.body.email || employeeToUpdate.email),
+    active: (req.body.active || employeeToUpdate.active),
   };
   if (!(employeeToUpdate)) {
     res.status(400).json({ msg: 'Please fill in a valid id' });
