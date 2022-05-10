c90f304b8fc62b7bf67ea84d6e2639a7ace1961d
Tobías Pujol <tobipujol@gmail.com>
2022-05-10 11:36:38 -0300

    TG: Update method added. fs.writeFile added in Post and Delete

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+const fs = require('fs');
 const admins = require('../data/admins.json');
 
 // Get all admins
@@ -19,19 +20,65 @@ router.get('/id=:id', (req, res) => {
 // Create admin
 router.post('/', (req, res) => {
   const rb = req.body;
-  if (!rb.id || !rb.firstName || !rb.lastName || !rb.email || !rb.password || !rb.active) {
+  if (!rb.id || !rb.firstName || !rb.lastName || !rb.email || !rb.password || (rb.active === '')) {
     res.status(400).json({ msg: 'Please include the solicited information' });
   }
   admins.push(req.body);
-  res.json(admins);
+  const adminsWithNew = admins;
+  fs.writeFile('src/data/admins.json', JSON.stringify(adminsWithNew), (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json({
+        msg: 'Admin created', admins: adminsWithNew,
+      });
+    }
+  });
 });
 
 // Delete admin
 router.delete('/id=:id', (req, res) => {
   const found = admins.some((admin) => admin.id === Number(req.params.id));
+  const restOfTheAdmins = admins.filter((admin) => admin.id !== Number(req.params.id));
   if (found) {
-    res.json({
-      msg: 'Admin deleted', admins: admins.filter((admin) => admin.id !== Number(req.params.id)),
+    fs.writeFile('src/data/admins.json', JSON.stringify(restOfTheAdmins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({
+          msg: 'Admin deleted', admins: restOfTheAdmins,
+        });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No admins with the id of ${req.params.id}` });
+  }
+});
+
+// Update admin
+router.put('/id=:id', (req, res) => {
+  const found = admins.some((admin) => admin.id === Number(req.params.id));
+  if (found) {
+    const restOfTheAdmins = admins.filter((admin) => admin.id !== Number(req.params.id));
+    const copyOfAdmin = admins.find((admin) => admin.id === Number(req.params.id));
+    const {
+      firstName, lastName, email, password, active,
+    } = req.body;
+    const updatedAdmin = {
+      id: Number(req.params.id),
+      firstName: (firstName || copyOfAdmin.firstName),
+      lastName: (lastName || copyOfAdmin.lastName),
+      email: (email || copyOfAdmin.email),
+      password: (password || copyOfAdmin.password),
+      active: Boolean(active ?? copyOfAdmin.active),
+    };
+    restOfTheAdmins.push(updatedAdmin);
+    fs.writeFile('src/data/admins.json', JSON.stringify(restOfTheAdmins), (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ msg: 'Admin updated', admins: restOfTheAdmins });
+      }
     });
   } else {
     res.status(400).json({ msg: `No admins with the id of ${req.params.id}` });
