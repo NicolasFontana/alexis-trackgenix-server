8772ec12894375b04df4054e93774c036423b504
Karen Agustina Soto <karenagustinams@gmail.com>
2022-05-16 10:12:52 -0300

    TG-07: Routes created

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
-import resAdmin from './controllers/admins';
-import employeesRoute from './controllers/employees';
-import projectRouter from './controllers/projects';
-import superAdmins from './controllers/super-admins';
-import taskRouter from './controllers/tasks';
-import timeSheets from './controllers/time-sheets';
+import router from './routes';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
+// database connection using mongoose
 const URI = 'mongodb+srv://pucheRR:BaSP2022@cluster0.3uv6a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
@@ -23,20 +18,16 @@ mongoose.connect(URI)
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// ROUTES
-app.use('/api/admins', resAdmin);
-app.use('/api/employees', employeesRoute);
-app.use('/api/projects', projectRouter);
-app.use('/api/super-admins', superAdmins);
-app.use('/api/tasks', taskRouter);
-app.use('/api/time-sheets', timeSheets);
+// ROUTES from src/routes
+app.use(router);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-// console.log(`Example app listening on port ${port}`);
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening on port ${port}`);
 });
 
 export default mongoose;
