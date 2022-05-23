5412e197a0bd8c5e76fcaa6a5405aedcb4b9e3cf
AnaMurinigo <anamurinigo@gmail.com>
2022-05-23 09:07:31 -0300

    TG-14: Seeds and test:get all successfull for Admin and Super Admin

---
+++ src/seed/admins.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('628ab4225aae617fa8002c21'),
+  firstName: 'Emilio',
+  lastName: 'Perez',
+  email: 'emilioPerez@mail.com',
+}];
