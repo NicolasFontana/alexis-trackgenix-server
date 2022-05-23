c9f160cb95ba75851b9a93286e80b501192a5799
AnaMurinigo <anamurinigo@gmail.com>
2022-05-22 22:18:40 -0300

    TG-16: Seeds and Get all success test

--- src/tests/projects.test.js
+++ src/tests/projects.test.js
@@ -1,6 +1,18 @@
-describe('void test', () => {
-  test('Dummy test', () => {
-    const num = 2;
-    expect(num).toBe(2);
+import request from 'supertest';
+import Projects from '../models/Projects';
+import projectsSeed from '../seed/projects';
+import app from '../app';
+
+beforeAll(async () => {
+  await Projects.collection.insertMany(projectsSeed);
+});
+
+describe('GET ALL project', () => {
+  test('All projects list', async () => {
+    const response = await (await request(app).get('/api/projects'));
+    expect(response.body.message).toBe('Success!');
+    expect(response.statusCode).toBe(200);
+    // expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.error).toBe(false);
   });
 });
