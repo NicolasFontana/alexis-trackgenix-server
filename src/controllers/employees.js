a8a856dd2416c72c5c2857f42caa421c26596cde
Javier Biselli <javierbiselli@gmail.com>
2022-05-23 20:10:08 -0300

    TG-19: Added tests for delete, added example test for getAll and create, modified models and validations, fixed controllers

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -3,7 +3,20 @@ import models from '../models';
 // get all employees
 const getAllEmployees = async (req, res) => {
   try {
-    const allEmployees = await models.Employees.find({});
+    const allEmployees = await models.Employees.find({})
+      .populate('projects', {
+        name: 1,
+        description: 1,
+        startDate: 1,
+        endDate: 1,
+        clientName: 1,
+        active: 1,
+      });
+      // .populate('timeSheets', {
+      //   projectId: 1,
+      //   taskId: 1,
+      //   approved: 1,
+      // });
     res.status(200).json({
       message: 'All employees',
       data: allEmployees,
@@ -22,7 +35,20 @@ const getAllEmployees = async (req, res) => {
 const getEmployeeById = async (req, res) => {
   try {
     if (req.params.id) {
-      const singleEmployee = await models.Employees.findById(req.params.id);
+      const singleEmployee = await models.Employees.findById(req.params.id)
+        .populate('projects', {
+          name: 1,
+          description: 1,
+          startDate: 1,
+          endDate: 1,
+          clientName: 1,
+          active: 1,
+        });
+        // .populate('timeSheets', {
+        //   projectId: 1,
+        //   taskId: 1,
+        //   approved: 1,
+        // });
       res.status(200).json({
         message: `Employee with id ${req.params.id}`,
         data: singleEmployee,
@@ -49,7 +75,20 @@ const getEmployeeByFirstName = async (req, res) => {
   try {
     if (req.params.firstName) {
       const firstNameParam = req.params.firstName;
-      const Employees = await models.Employees.find({ firstName: firstNameParam });
+      const Employees = await models.Employees.find({ firstName: firstNameParam })
+        .populate('projects', {
+          name: 1,
+          description: 1,
+          startDate: 1,
+          endDate: 1,
+          clientName: 1,
+          active: 1,
+        });
+        // .populate('timeSheets', {
+        //   projectId: 1,
+        //   taskId: 1,
+        //   approved: 1,
+        // });
       res.status(200).json({
         message: `Employee with firstName ${firstNameParam}`,
         data: Employees,
@@ -76,7 +115,20 @@ const getEmployeeByLastName = async (req, res) => {
   try {
     if (req.params.lastName) {
       const lastNameParam = req.params.lastName;
-      const Employees = await models.Employees.find({ lastName: lastNameParam });
+      const Employees = await models.Employees.find({ lastName: lastNameParam })
+        .populate('projects', {
+          name: 1,
+          description: 1,
+          startDate: 1,
+          endDate: 1,
+          clientName: 1,
+          active: 1,
+        });
+        // .populate('timeSheets', {
+        //   projectId: 1,
+        //   taskId: 1,
+        //   approved: 1,
+        // });
       res.status(200).json({
         message: `Employee with lastName ${lastNameParam}`,
         data: Employees,
@@ -103,7 +155,20 @@ const getEmployeeByActivity = async (req, res) => {
   try {
     if (req.params.active) {
       const activeParam = req.params.active;
-      const Employees = await models.Employees.find({ active: activeParam });
+      const Employees = await models.Employees.find({ active: activeParam })
+        .populate('projects', {
+          name: 1,
+          description: 1,
+          startDate: 1,
+          endDate: 1,
+          clientName: 1,
+          active: 1,
+        })
+        .populate('timeSheets', {
+          projectId: 1,
+          taskId: 1,
+          approved: 1,
+        });
       res.status(200).json({
         message: `Employee with status ${activeParam}`,
         data: Employees,
@@ -133,7 +198,10 @@ const createEmployee = async (req, res) => {
       lastName: req.body.lastName,
       phone: req.body.phone,
       email: req.body.email,
+      password: req.body.password,
       active: req.body.active,
+      projects: req.body.projects,
+      timesheets: req.body.timesheets,
     });
     const result = await employee.save();
     return res.status(201).json({
@@ -165,7 +233,19 @@ const updateEmployee = async (req, res) => {
       req.params.id,
       req.body,
       { new: true },
-    );
+    ).populate('projects', {
+      name: 1,
+      description: 1,
+      startDate: 1,
+      endDate: 1,
+      clientName: 1,
+      active: 1,
+    })
+      .populate('timeSheets', {
+        projectId: 1,
+        taskId: 1,
+        approved: 1,
+      });
     if (!result) {
       res.status(404).json({
         message: 'The employee has not been found',
@@ -199,22 +279,22 @@ const deleteEmployee = async (req, res) => {
     }
     const result = await models.Employees.findByIdAndDelete(req.params.id);
     if (!result) {
-      res.status(404).json({
-        message: 'The employee has not been found',
+      return res.status(404).json({
+        message: 'Employee not found',
         data: undefined,
         error: true,
       });
     }
-    res.json({
-      message: 'The employee has been succesfully deleted',
+    return res.status(200).json({
+      message: `Employee id: ${req.params.id} deleted.`,
       data: result,
       error: false,
-    }).status(204);
-  } catch (err) {
-    res.status(400).json({
-      message: err,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
       data: undefined,
-      err: true,
+      error: true,
     });
   }
 };
--- src/tests/employees.test.js
+++ src/tests/employees.test.js
@@ -1,6 +1,65 @@
-describe('void test', () => {
-  test('Dummy test', () => {
-    const num = 2;
-    expect(num).toBe(2);
+/* eslint-disable no-underscore-dangle */
+
+import request from 'supertest';
+import app from '../app';
+import Employees from '../models/Employees';
+import EmployeesSeed from '../seed/employees';
+
+beforeAll(async () => {
+  await Employees.collection.insertMany(EmployeesSeed);
+});
+
+let employeeId;
+
+describe('Get all employees', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/api/employees').send();
+    expect(response.status).toBe(200);
+  });
+});
+
+describe('Create employee', () => {
+  test('response should return a false error', async () => {
+    const response = await request(app).post('/api/employees/').send({
+      firstName: 'Puche',
+      lastName: 'Lopez',
+      phone: 7761785000,
+      email: 'juanssssopez@people.com.cn',
+      password: 'tuvieja123123',
+      active: false,
+      projects: [
+        '62883891a6c3e40d965f7f8c',
+      ],
+      timeSheets: [
+        '62883891a6c3e40d965f7f8c',
+      ],
+    });
+    expect(response.body.error).toBeFalsy();
+    employeeId = response.body.data._id;
+  });
+});
+
+describe('Delete employee', () => {
+  test('response should return a true error', async () => {
+    const response = await request(app).delete('/api/employees/').send();
+    expect(response.error).toBeTruthy();
+  });
+
+  test('response should be successfull ', async () => {
+    const response = await request(app).delete(`/api/employees/${employeeId}`).send();
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toEqual(`Employee id: ${employeeId} deleted.`);
+    expect(response.status).toBe(200);
+    employeeId = response.body.data._id;
+  });
+
+  test('response should now return a 404 status', async () => {
+    const response = await request(app).delete(`/api/employees/${employeeId}`).send();
+    expect(response.status).toEqual(404);
+  });
+
+  test('response should now return "Employee not found"', async () => {
+    const response = await request(app).delete(`/api/employees/${employeeId}`).send();
+    expect(response.body.message).toEqual('Employee not found');
   });
 });
