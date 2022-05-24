48361b97c1a8fb47392873b9d45b6ae772051a67
Fernando Carbone <fernando.carbone707@gmail.com>
2022-05-24 20:37:24 -0300

    TG-20: Adding msg errors from testing

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -13,19 +13,22 @@ beforeAll(async () => {
 describe('GET /api/time-sheets', () => {
   test('Response should return a 200 status', async () => {
     const response = await request(app).get('/api/time-sheets').send();
-    await expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Time-Sheets');
+    expect(response.body.data).toEqual(expect.any(Object));
+    expect(response.body.error).toBeFalsy();
   });
-  test('response should return false error', async () => {
+  test('Wrong path', async () => {
     const response = await request(app).get('/api/time/sheets').send();
-    await expect(response.status).toBe(404);
+    expect(response.status).toBe(404);
   });
-  test('response should return false error', async () => {
+  test('Wrong path', async () => {
     const response = await request(app).get('/api/timesheets').send();
-    await expect(response.status).toBe(404);
+    expect(response.status).toBe(404);
   });
-  test('response should return false error', async () => {
+  test('Wrong path', async () => {
     const response = await request(app).get('/api').send();
-    await expect(response.status).toBe(404);
+    expect(response.status).toBe(404);
   });
 });
 
@@ -42,26 +45,52 @@ describe('POST /api/time-sheets', () => {
       approved: true,
     });
     expect(response.status).toBe(201);
+    expect(response.body.msg).toBe('Timesheet created');
+    expect(response.body.data).toEqual(expect.any(Object));
+    expect(response.body.error).toBeFalsy();
     // eslint-disable-next-line no-underscore-dangle
     timesheetId = response.body.data._id;
   });
-});
-
-// Test for PUT method by Fran
-
-describe('PUT /api/time-sheets', () => {
-  test('Should update a timesheet', async () => {
-    const response = await request(app).put('/api/time-sheets/6289c467fc13ae72d60000c7').send({
+  test('Should fill all required fields', async () => {
+    const response = await request(app).post('/api/time-sheets').send({
       projectId: '6289c467fc13ae72d60000c9',
       Task: [
         {
-          taskId: '6289c467fc13ae72d60000ca',
+          taskId: '6289c467fc13ae72d60000cb',
+        },
+      ],
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe('There was an error during the validation of the request');
+    expect(response.body.error.length).toBeGreaterThan(3);
+  });
+  test('Must not pass projectId validation', async () => {
+    const response = await request(app).post('/api/time-sheets').send({
+      projectId: '6289c467fc13ae72d',
+      Task: [
+        {
+          taskId: '6289c467fc13ae72d60000cb',
         },
       ],
       approved: true,
     });
-    expect(response.status).toBe(200);
-    expect(response.body.error).toBeFalsy();
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe('There was an error during the validation of the request');
+    expect(response.body.error.length).toBeGreaterThan(3);
+  });
+  test('Must not pass taskId validation', async () => {
+    const response = await request(app).post('/api/time-sheets').send({
+      projectId: '6289c467fc13ae72d60000c9',
+      Task: [
+        {
+          taskId: '6289c467fc13ae7',
+        },
+      ],
+      approved: true,
+    });
+    expect(response.status).toBe(400);
+    expect(response.body.msg).toBe('There was an error during the validation of the request');
+    expect(response.body.error.length).toBeGreaterThan(3);
   });
 });
 
@@ -73,9 +102,10 @@ describe('DELETE /api/time-sheets', () => {
     expect(response.status).toEqual(200);
     expect(response.body.msg).toBe(`The ${timesheetId} timesheet has been susccesfully deleted`);
   });
-  test('Should not delete a proyect ', async () => {
+  test('Should not delete a project ', async () => {
     const response = await request(app).delete('/api/time-sheets/6280062d5f0b9b4131e527e4').send();
     expect(response.status).toEqual(404);
     expect(response.body.msg).toBe('There is no timesheet with this Id 6280062d5f0b9b4131e527e4');
+    expect(response.body.error).toBeTruthy();
   });
 });
