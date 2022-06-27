d22c4b74f4b93ed0f68d4359f65d9e42426d3e68
Iara Criscenti <iaracriscenti@gmail.com>
2022-06-27 13:40:33 -0300

    TG-90: Updated schemas and routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import superAdminRoutes from './super-admins';
 import employeeRoutes from './employees';
 import projectRoutes from './projects';
 import timeSheetsRoutes from './time-sheets';
+import auth from './auth';
 
 const router = express.Router();
 
@@ -14,6 +15,7 @@ router
   .use('/api/super-admins', superAdminRoutes)
   .use('/api/employees', employeeRoutes)
   .use('/api/projects', projectRoutes)
-  .use('/api/time-sheets', timeSheetsRoutes);
+  .use('/api/time-sheets', timeSheetsRoutes)
+  .use('/api/auth', auth);
 
 export default router;
